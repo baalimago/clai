02ed45511da563cc311b4a92d8f994cb2d74cbef
imago <l.kinde94@outlook.com>
2024-03-01 07:43:05 +0200

    Cleaned up solution

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/baalimago/go_away_boilerplate/pkg/ancli"
+	"github.com/baalimago/go_away_boilerplate/pkg/shutdown"
 )
 
 const usage = `Goai - Go do AI stuff
@@ -40,7 +41,7 @@ func errorOnMutuallyExclusiveFlags(flag1, flag2, shortFlag, longFlag, defualt st
 	return defualt
 }
 
-func main() {
+func setup() (string, string, string, string) {
 	chatModelDefault := "gpt-4-turbo-preview"
 	cmShort := flag.String("cm", chatModelDefault, "Set the chat model to use. Default is gpt-4-turbo-preview. Mutually exclusive with chat-model flag.")
 	cmLong := flag.String("chat-model", chatModelDefault, "Set the chat model to use. Default is gpt-4-turbo-preview. Mutually exclusive with cm flag.")
@@ -49,9 +50,10 @@ func main() {
 	pmShort := flag.String("pm", photoModelDefault, "Set the image model to use. Default is dall-e-3. Mutually exclusive with photo-model flag.")
 	pmLong := flag.String("photo-model", photoModelDefault, "Set the image model to use. Default is dall-e-3. Mutually exclusive with pm flag.")
 
-	pictureDirDefault := "./pictures"
-	pdShort := flag.String("pd", pictureDirDefault, "Set the directory to store the generated pictures. Default is ./pictures")
-	pdLong := flag.String("picture-dir", pictureDirDefault, "Set the directory to store the generated pictures. Default is ./pictures")
+	home := os.Getenv("HOME")
+	pictureDirDefault := fmt.Sprintf("%v/Pictures", home)
+	pdShort := flag.String("pd", pictureDirDefault, "Set the directory to store the generated pictures. Default is $HOME/Pictures")
+	pdLong := flag.String("picture-dir", pictureDirDefault, "Set the directory to store the generated pictures. Default is $HOME/Pictures")
 
 	flag.Parse()
 	chatModel := errorOnMutuallyExclusiveFlags(*cmShort, *cmLong, "cm", "chat-model", chatModelDefault)
@@ -63,7 +65,39 @@ func main() {
 		ancli.PrintErr("OPENAI_API_KEY environment variable not set\n")
 		os.Exit(1)
 	}
+	return chatModel, photoModel, pictureDir, API_KEY
+}
 
+func run(ctx context.Context, args []string, chatModel, photoModel, pictureDir, API_KEY string) error {
+	switch args[0] {
+	case "text":
+		fallthrough
+	case "t":
+		err := queryChatModel(ctx, chatModel, API_KEY, args[1:])
+		if err != nil {
+			return fmt.Errorf("failed to query chat model: %w", err)
+		}
+	case "photo":
+		fallthrough
+	case "p":
+		pq := photoQuerier{
+			model:      photoModel,
+			API_KEY:    API_KEY,
+			pictureDir: pictureDir,
+		}
+		err := pq.queryPhotoModel(ctx, args[1:])
+		if err != nil {
+			return fmt.Errorf("failed to query photo model: %w", err)
+		}
+	default:
+		return fmt.Errorf("unknown command: %s", args[0])
+	}
+
+	return nil
+}
+
+func main() {
+	chatModel, photoModel, pictureDir, API_KEY := setup()
 	args := flag.Args()
 	if len(args) == 0 {
 		ancli.PrintErr("No command specified")
@@ -71,26 +105,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	ctx := context.Background()
-	switch args[0] {
-	case "text":
-		fallthrough
-	case "t":
-		err := queryChatModel(ctx, chatModel, API_KEY, args[1:])
+	ctx, cancel := context.WithCancel(context.Background())
+	go shutdown.Monitor(cancel)
+	go func() {
+		err := run(ctx, args, chatModel, photoModel, pictureDir, API_KEY)
 		if err != nil {
 			ancli.PrintErr(err.Error() + "\n")
 			os.Exit(1)
 		}
-	case "photo":
-		fallthrough
-	case "p":
-		err := queryPhotoModel(ctx, photoModel, API_KEY, pictureDir, args[1:])
-		if err != nil {
-			ancli.PrintErr(err.Error() + "\n")
-			os.Exit(1)
-		}
-	default:
-		ancli.PrintErr(fmt.Sprintf("Unknown command: %s\n", args[0]))
-		os.Exit(1)
-	}
+		cancel()
+	}()
+
+	<-ctx.Done()
+	ancli.PrintOK("things seems to have worked out. Good bye! 🚀\n")
 }
--- photo.go
+++ photo.go
@@ -36,11 +36,14 @@ type ImageResponses struct {
 	Data    []ImageResponse `json:"data"`
 }
 
-// queryPhotoModel using the supplied arguments as instructions
-func queryPhotoModel(ctx context.Context, model, API_KEY, pictureDir string, text []string) error {
+type photoQuerier struct {
+	model, API_KEY, pictureDir string
+}
+
+func (pq *photoQuerier) query(ctx context.Context, text []string) (ImageResponses, error) {
 	url := "https://api.openai.com/v1/images/generations"
 	body := imageQuery{
-		Model:          model,
+		Model:          pq.model,
 		Prompt:         fmt.Sprintf("I NEED to test how the tool works with extremely simple prompts. DO NOT add any detail, just use it AS-IS: %v", strings.Join(text, " ")),
 		N:              1,
 		Size:           "1024x1024",
@@ -50,45 +53,63 @@ func queryPhotoModel(ctx context.Context, model, API_KEY, pictureDir string, tex
 	}
 	bodyBytes, err := json.Marshal(body)
 	if err != nil {
-		return fmt.Errorf("failed to encode JSON: %w", err)
+		return ImageResponses{}, fmt.Errorf("failed to encode JSON: %w", err)
 	}
 
 	req, err := http.NewRequestWithContext(ctx, "POST", url, bytes.NewBuffer(bodyBytes))
 	if err != nil {
-		return fmt.Errorf("failed to create request: %w", err)
+		return ImageResponses{}, fmt.Errorf("failed to create request: %w", err)
 	}
 
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", API_KEY))
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", pq.API_KEY))
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return fmt.Errorf("failed tosending request: %w", err)
+		return ImageResponses{}, fmt.Errorf("failed tosending request: %w", err)
 	}
 	defer resp.Body.Close()
 
 	bytes, err := io.ReadAll(resp.Body)
 	if resp.StatusCode != 200 {
-		return fmt.Errorf("non-OK status: %v, body: %v", resp.Status, string(bytes))
+		return ImageResponses{}, fmt.Errorf("non-OK status: %v, body: %v", resp.Status, string(bytes))
 	}
 	var imgResps ImageResponses
 	err = json.Unmarshal(bytes, &imgResps)
 	if err != nil {
-		return fmt.Errorf("failed to decode JSON: %w", err)
+		return ImageResponses{}, fmt.Errorf("failed to decode JSON: %w", err)
 	}
+	return imgResps, nil
+}
 
-	for _, imgResp := range imgResps.Data {
-		data, err := base64.StdEncoding.DecodeString(imgResp.B64_JSON)
-		if err != nil {
-			return fmt.Errorf("failed to decode base64: %w", err)
-		}
-		outFile := fmt.Sprintf("%v/goai_%v.jpg", pictureDir, randomPrefix())
+func (pq *photoQuerier) saveImage(ctx context.Context, imgResp ImageResponse) error {
+	data, err := base64.StdEncoding.DecodeString(imgResp.B64_JSON)
+	if err != nil {
+		return fmt.Errorf("failed to decode base64: %w", err)
+	}
+	outFile := fmt.Sprintf("%v/goai_%v.jpg", pq.pictureDir, randomPrefix())
+	err = os.WriteFile(outFile, data, 0644)
+	if err != nil {
+		ancli.PrintWarn(fmt.Sprintf("failed to write file: '%v', attempting tmp file...\n", err))
+		outFile = fmt.Sprintf("/tmp/goai_%v.jpg", randomPrefix())
 		err = os.WriteFile(outFile, data, 0644)
 		if err != nil {
 			return fmt.Errorf("failed to write file: %w", err)
 		}
-		ancli.PrintOK(fmt.Sprintf("revised prompt: '%v'\nimage rendered to: '%v'", imgResp.RevisedPrompt, outFile))
+	}
+	ancli.PrintOK(fmt.Sprintf("revised prompt: '%v'\nimage rendered to: '%v'\n", imgResp.RevisedPrompt, outFile))
+	return nil
+}
+
+// queryPhotoModel using the supplied arguments as instructions
+func (pq *photoQuerier) queryPhotoModel(ctx context.Context, text []string) error {
+	imgResps, err := pq.query(ctx, text)
+	if err != nil {
+		return err
+	}
+	for _, imgResp := range imgResps.Data {
+		pq.saveImage(ctx, imgResp)
 	}
 	return nil
 }
