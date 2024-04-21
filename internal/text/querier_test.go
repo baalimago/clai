5089b97e985d44f4b6c8bf3fe6819f16da02f9af
imago <l.kinde94@outlook.com>
2024-04-21 20:03:04 +0300

    Re-added tests

--- internal/text/querier_test.go
+++ internal/text/querier_test.go
@@ -134,29 +134,29 @@ func Test_Querier_eventHandling(t *testing.T) {
 		given []models.CompletionEvent
 		want  string
 	}{
-		// {
-		// 	desc: "it should print token to stdout",
-		// 	q: Querier[*MockQuerier]{
-		// 		Raw: true,
-		// 		Model: &MockQuerier{
-		// 			completionChan: make(chan models.CompletionEvent),
-		// 		},
-		// 	},
-		// 	given: []models.CompletionEvent{"test", "CLOSE"},
-		// 	want:  "test",
-		// },
-		// {
-		// 	desc: "token whitespace should be respected",
-		// 	q: Querier[*MockQuerier]{
-		// 		Raw: true,
-		// 		Model: &MockQuerier{
-		// 			completionChan: make(chan models.CompletionEvent),
-		// 		},
-		// 	},
-		// 	given: []models.CompletionEvent{" one", "two\n", "  three ", "CLOSE"},
-		// 	want: ` onetwo
-		// three `,
-		// },
+		{
+			desc: "it should print token to stdout",
+			q: Querier[*MockQuerier]{
+				Raw: true,
+				Model: &MockQuerier{
+					completionChan: make(chan models.CompletionEvent),
+				},
+			},
+			given: []models.CompletionEvent{"test", "CLOSE"},
+			want:  "test",
+		},
+		{
+			desc: "token whitespace should be respected",
+			q: Querier[*MockQuerier]{
+				Raw: true,
+				Model: &MockQuerier{
+					completionChan: make(chan models.CompletionEvent),
+				},
+			},
+			given: []models.CompletionEvent{" one", "two\n", "  three ", "CLOSE"},
+			want: ` onetwo
+		three `,
+		},
 		{
 			desc: "it should call test function when asked to",
 			q: Querier[*MockQuerier]{
