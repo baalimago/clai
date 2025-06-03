6319765cf52c8adbb771a996a83df6584590cd4f
imago <l.kinde94@outlook.com>
2025-06-03 20:34:40 +0300

    feat: Added new freetext tool which allows the llm to run anything
    This tool should be added with caution, it may rm -rfd whatever it feels
    like without confirmation

---
+++ internal/tools/bash_tool_freetext_command.go
@@ -0,0 +1,47 @@
+package tools
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+type FreetextCmdTool UserFunction
+
+var FreetextCmd = FreetextCmdTool{
+	Name:        "freetext_command",
+	Description: "Run any entered string as a terminal command.",
+	Inputs: &InputSchema{
+		Type: "object",
+		Properties: map[string]ParameterObject{
+			"command": {
+				Type:        "string",
+				Description: "The freetext comand. May be any string. Will return error on non-zero exit code.",
+			},
+		},
+		Required: []string{"command"},
+	},
+}
+
+func (r FreetextCmdTool) Call(input Input) (string, error) {
+	freetextCmd, ok := input["command"].(string)
+	if !ok {
+		return "", fmt.Errorf("freetextCmd must be a string")
+	}
+	freetextCmdSplit := strings.Split(freetextCmd, " ")
+	var potentialArgsFlags []string
+	if len(freetextCmdSplit) > 0 {
+		potentialArgsFlags = freetextCmdSplit[1:]
+	}
+	cmd := exec.Command(freetextCmdSplit[0], potentialArgsFlags...)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("error: '%w', output: %v", err, string(output))
+	}
+	return string(output), nil
+}
+
+func (r FreetextCmdTool) UserFunction() UserFunction {
+	return UserFunction(FreetextCmd)
+}
