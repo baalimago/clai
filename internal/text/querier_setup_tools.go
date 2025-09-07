494e77b197b557a743e36d18318fdc86e3df7201
imago <l.kinde94@outlook.com>
2025-09-07 19:59:50 +0300

    fix: Profiles no longer use tools when set to false

--- internal/text/querier_setup_tools.go
+++ internal/text/querier_setup_tools.go
@@ -123,7 +123,7 @@ func addMcpTools(ctx context.Context, mcpServersDir string, userConf Configurati
 
 func setupTooling[C models.StreamCompleter](ctx context.Context, modelConf C, userConf Configurations) {
 	toolBox, ok := any(modelConf).(models.ToolBox)
-	if ok && (userConf.UsingProfile() || userConf.UseTools) {
+	if ok && userConf.UseTools {
 		tools.Init()
 		// Only setup MCP tools if they're there's a chance of using tools
 		err := addMcpTools(ctx, path.Join(userConf.ConfigDir, "mcpServers"), userConf)
