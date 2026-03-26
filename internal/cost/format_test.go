a796917882a298f8c9e334804ed6bcd2bd2d8f70
imago <l.kinde94@outlook.com>
2026-03-26 21:13:14 +0200

    feat: Increase precision for cost
    I usually have conversations around $0.01 etc, which always rounds to 0.
    Now I can see!

--- internal/cost/format_test.go
+++ internal/cost/format_test.go
@@ -8,6 +8,7 @@ func TestFormatUSD(t *testing.T) {
 		want string
 	}{
 		{in: 0, want: "$0"},
+		{in: 0.001, want: "$0.001"},
 		{in: 0.01, want: "$0.01"},
 		{in: 1.2, want: "$1.2"},
 		{in: 14.53, want: "$14.53"},
